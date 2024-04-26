ef3ee08f91d9d7dff7d7b76600e086a66a316682
Vipin Agrawal <vipin.agrawal@northwestern.edu>
2024-04-25 19:48:34 -0500

    now stretch coefficient is constant throughout the membrane

--- src/init.cpp
+++ src/init.cpp
@@ -325,31 +325,29 @@ void write_parameters(MBRANE_p mbrane, MC_p mc_para, AREA_p area_para, FLUID_p f
 
     out_.close();
 }
-
+//
 void init_KK_0(double *KK, AREA_p area_p, MESH_p mesh, int N){
     int i, k;
     int num_nbr, cm_idx;
-    double kk_t = area_p.YY;
+    double kk_t = area_p.YY*sqrt(3)/2;
     int mpi_err, mpi_rank;
-    double rndg;
+    // double rndg;
     /* std::cout << "Gaussian Random Number: " << gaussianNumber << std::endl; */
-
-
     mpi_err =  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
     /* std::uniform_real_distribution<> rand_real(-kk_t/4.0, kk_t/4); */
     for(i = 0; i < N; i++){
         num_nbr = mesh.numnbr[i];
         cm_idx = mesh.nghst * i;
         for(k = cm_idx; k < cm_idx + num_nbr; k++) {
-            rndg = RandomGenerator::generateGaussian(0.0,1.0);
-            KK[k] = kk_t + sqrt(kk_t)*rndg*0.5; //rand_real(rng2);
+            // rndg = RandomGenerator::generateGaussian(0.0,1.0);
+            KK[k] = kk_t;
+            // + sqrt(kk_t)*rndg*0.5; //rand_real(rng2);
             /* printf("%d  %lf \n", mpi_rank, KK[k]); */
         }
     }
     /* exit(0); */
 }
-
-
+//
 void init_activity(ACTIVE_p activity, int N){
     int i;
     if(activity.act == "random"){
